61f7514c413b9798cdf1bb95ba96b2108a25302f
Sazzad Hossain <hossain.sakib26@hotmail.com>
2023-01-02 12:50:20 +0600

    tour of hero display list adn details portion done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
+// import 'animate.css';
 
 @NgModule({
   declarations: [
